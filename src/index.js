3f1e7032112d02b5c7ed0c17081d4795a6575aa3
Rohan Bagchi <rohan.bagchi01@gmail.com>
2021-07-25 14:33:31 +0530

    Adding github ribbon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
+import './github-ribbon.css';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
+    <a
+      className="gh-ribbon darkblue right"
+      href="https://github.com/rohanBagchi/devto-testing"
+      target="_blank"
+    >
+      View on GitHub!
+    </a>
     <App />
   </StrictMode>,
   rootElement
